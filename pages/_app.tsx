59d392dfd4964ee8f1b026036579cdaaff307e95
Graham Tackley <gtackley@brave.com>
2021-07-27 04:11:46 +0100

    Update to MUI 5 beta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,14 +5,6 @@ import React from "react";
 import theme from "../src/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles) {
-      jssStyles.parentElement?.removeChild(jssStyles);
-    }
-  }, []);
-
   return (
     <React.Fragment>
       <Head>
@@ -29,7 +21,5 @@ function MyApp({ Component, pageProps }: AppProps) {
       </ThemeProvider>
     </React.Fragment>
   );
-
-  return <Component {...pageProps} />;
 }
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,37 +1,8 @@
-import { ServerStyleSheets } from "@material-ui/core";
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-} from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const sheets = new ServerStyleSheets();
-    const originalRenderPage = ctx.renderPage;
-
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-      });
-
-    const initialProps = await Document.getInitialProps(ctx);
-
-    return {
-      ...initialProps,
-      // Styles fragment is rendered after the app and page rendering finish.
-      styles: [
-        ...React.Children.toArray(initialProps.styles),
-        sheets.getStyleElement(),
-      ],
-    };
-  }
-
   render() {
-    console.log("MyDocument running");
     return (
       <Html>
         <Head>
